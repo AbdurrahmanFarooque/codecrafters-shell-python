a0c595b5e140681b5fa942536879f9c54e1e484d
Abdurrahman Farooque <abdurrahman.f24@gmail.com>
2026-05-31 15:52:25 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,14 +2,27 @@ import sys
 
 
 def main():
+    # Assume input is correct
+    def extractCommandAndText(userInput):
+        wordList = userInput.split()
+        command = wordList[0]
+        text = ' '.join(wordList[1: ])
+        return command, text
+ 
     exit = False
     while not exit:
         sys.stdout.write("$ ")
-        command = input()
+        userInput = input()
+
+        command, text = extractCommandAndText(userInput)
+
         if command == 'exit':
             exit = True
+        elif command == 'echo':  
+            print(text)
         else:
             print(f'{command}: command not found')
+
 
 
 if __name__ == "__main__":
