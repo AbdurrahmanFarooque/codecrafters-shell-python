0f1432a90da810022d4684534c6638aa16f55f14
Abdurrahman Farooque <abdurrahman.f24@gmail.com>
2026-05-31 16:46:37 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,11 +4,23 @@ import sys
 def main():
     # Assume input is correct
     def extractCommandAndText(userInput):
-        wordList = userInput.split()
+        wordList = userInput.split(maxsplit = 1)
+
+        if len(wordList) == 1:
+            command = wordList[0]
+            text = ''
+            return command, text
+        
         command = wordList[0]
-        text = ' '.join(wordList[1: ])
+        text = wordList[1]
         return command, text
  
+    builtins = [
+        'exit',
+        'echo',
+        'type',
+    ]
+
     exit = False
     while not exit:
         sys.stdout.write("$ ")
@@ -20,6 +32,11 @@ def main():
             exit = True
         elif command == 'echo':  
             print(text)
+        elif command == 'type':
+            if text in builtins:
+                print(f'{text} is a shell builtin')
+            else:
+                print(f'{text}: not found')
         else:
             print(f'{command}: command not found')
 
